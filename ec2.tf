eec086d341582e39828e0c6f0d00c9c9399ce7d5
Pullarao Pantangi <46434781+PullaPantangi@users.noreply.github.com>
2025-07-14 18:46:45 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,23 @@
+resource "aws_instance" "Public_server_1" {
+  ami                         = "ami-0150ccaf51ab55a51"
+  instance_type               = "t2.micro"
+  key_name                    = "test"
+
+  vpc_security_group_ids      = [data.terraform_remote_state.base_infra.outputs.sg1]
+  subnet_id                   = data.terraform_remote_state.base_infra.outputs.sub_id
+  associate_public_ip_address = true
+  #availability_zone = "us-east-1a"
+  #    user_data = <<  EOF
+  #    #!/bin/bash
+  #sudo sed -i 's/#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_config
+  #sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+  #sudo systemctl restart sshd.service
+  #sudo sed -i 's/disable_root: true/disable_root: false/g' /etc/cloud/cloud.cfg
+  #sudo echo "root:abc123" | sudo chpasswd
+  #EOF
+
+  tags = {
+    Name    = "Public_server_1"
+    Account = "RR"
+  }
+}
